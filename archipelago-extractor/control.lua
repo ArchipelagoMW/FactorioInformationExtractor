a03f92ba74595f7863943177383cbc33c6670a67
CaitSith2 <d_good@caitsith2.com>
2021-11-11 02:21:47 -0800

    Extract resource mining data

--- archipelago-extractor/control.lua
+++ archipelago-extractor/control.lua
@@ -52,13 +52,58 @@ function dumpGameInfo()
     game.print("Exported Recipe Data")
     data_collection = {}
     for _, proto in pairs(game.entity_prototypes) do
-        if proto.crafting_categories then
-            data_collection[proto.name] = proto.crafting_categories
+        if proto.crafting_categories or proto.resource_categories then
+            data_collection[proto.name] = {}
+            if proto.crafting_categories then
+                data_collection[proto.name]["crafting"] = {}
+                data_collection[proto.name]["crafting"]["speed"] = proto.crafting_speed
+                data_collection[proto.name]["crafting"]["categories"] = proto.crafting_categories
+            end
+            if proto.resource_categories then
+                data_collection[proto.name]["mining"] = {}
+                data_collection[proto.name]["mining"]["categories"] = proto.resource_categories
+                data_collection[proto.name]["mining"]["speed"] = proto.mining_speed
+                data_collection[proto.name]["mining"]["radius"] = proto.mining_drill_radius
+            end
         end
     end
-
     game.write_file("machines.json", game.table_to_json(data_collection), false)
     game.print("Exported Machine Data")
+    
+    data_collection = {}
+    for _, proto in pairs(game.autoplace_control_prototypes) do
+        if proto.category == "resource" then
+            local r_proto = game.entity_prototypes[proto.name]
+            local minable = r_proto.mineable_properties
+            local resource = {}
+            resource["minable"] = minable.minable
+            resource["infinite"] = r_proto.infinite_resource
+            if r_proto.infinite_resource then
+                resource["infinite_depletion"] = r_proto.infinite_depletion_resource_amount
+            end
+            resource["category"] = r_proto.resource_category
+            resource["mining_time"] = minable.mining_time
+            resource["required_fluid"] = minable.required_fluid
+            resource["fluid_amount"] = minable.fluid_amount
+            resource["products"] = {}
+            for _, product in pairs(minable.products) do
+                resource["products"][product.name] = {}
+                resource["products"][product.name]["type"] = product.type
+                resource["products"][product.name]["name"] = product.name
+                resource["products"][product.name]["amount"] = product.amount
+                resource["products"][product.name]["amount_min"] = product.amount_min
+                resource["products"][product.name]["amount_max"] = product.amount_max
+                resource["products"][product.name]["probability"] = product.probability
+                resource["products"][product.name]["catalyst_amount"] = product.catalyst_amount
+                if product.type == "fluid" then
+                    resource["products"][product.name]["temperature"] = product.temperature
+                end
+            end
+            data_collection[proto.name] = resource
+        end
+    end
+    game.write_file("resources.json", game.table_to_json(data_collection), false)
+    game.print("Exported Minable Resource Data")
 end
 
 commands.add_command("ap-get-info-dump", "Dump Game Info, used by Archipelago.", function(call)
