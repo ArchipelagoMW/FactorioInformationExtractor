42b29e97ed7b5545e9a2f13cf429ca68a9623c52
Fabian Dill <fabian.dill@web.de>
2024-11-05 23:47:04 +0100

    strip ingredients and trigger

--- archipelago-extractor/control.lua
+++ archipelago-extractor/control.lua
@@ -3,7 +3,6 @@ function dumpTechInfo(force)
 
     for tech_name, tech in pairs(force.technologies) do
         if tech.enabled and tech.research_unit_count_formula == nil then
-			local tech_proto = tech.prototype
             local tech_data = {}
             local unlocks = {}
             tech_data["unlocks"] = unlocks
@@ -21,22 +20,13 @@ function dumpTechInfo(force)
                     tech_data["has_modifier"] = true
                 end
             end
-			
-			local ingredients = {}
-			tech_data["ingredients"] = ingredients
-			for _, ingredient in pairs(tech_proto.research_unit_ingredients) do
-				table.insert(ingredients, ingredient.name)
-			end
-			
-			if tech_proto.research_trigger then
-				tech_data["trigger"] = tech_proto.research_trigger
-			end
+
             data_collection[tech_name] = tech_data
             tech.researched = true -- enable all available recipes
         end
-        helpers.write_file("techs.json", helpers.table_to_json(data_collection), false)
-        game.print("Exported Tech Data")
     end
+    helpers.write_file("techs.json", helpers.table_to_json(data_collection), false)
+    game.print("Exported Tech Data")
 end
 
 function dumpRecipeInfo(force)
